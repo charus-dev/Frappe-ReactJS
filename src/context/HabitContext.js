47fea226963adfed52371705782591c8cc888f1d
Charu Saraswat <128489200+charus-dev@users.noreply.github.com>
2024-12-26 15:04:07 +0530

    Added HabitContext

---
+++ src/context/HabitContext.js
@@ -0,0 +1,35 @@
+import React, { createContext, useContext, useState } from "react";
+
+const HabitContext = createContext();
+
+export const HabitProvider = ({ children }) => {
+    const [habits, setHabits] = useState([]);
+
+    const addHabit = (habit) => {
+        setHabits((prevHabits) => [...prevHabits, habit]);
+    };
+
+    const updateHabit = (id, updatedName) => {
+        setHabits((prevHabits) =>
+            prevHabits.map((habit) =>
+                habit.id === id ? { ...habit, name: updatedName } : habit
+            )
+        );
+    };
+
+    const deleteHabit = (id) => {
+        setHabits((prevHabits) => prevHabits.filter((habit) => habit.id !== id));
+    };
+
+    return (
+        <HabitContext.Provider
+            value={{ habits, addHabit, updateHabit, deleteHabit }}
+        >
+            {children}
+        </HabitContext.Provider>
+    );
+};
+
+export const useHabits = () => {
+    return useContext(HabitContext);
+};
